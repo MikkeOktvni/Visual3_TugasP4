38c3480d1d5bb981f588c5ae883ff92d08912206
MikkeOktvni <oktvnimikke@gmail.com>
2024-10-18 21:13:03 +0700

    Delete Mikke Oktaviani_TugasP4 directory

--- Mikke Oktaviani_TugasP4/PYQT5/Latihan1.py
+++
@@ -1,7 +0,0 @@
-# QtGui QTCore QtWidgets
-# ============== Cara 1 ==========================================
-from PyQt5 import QtGui, QtCore, QtWidgets
-app = QtWidgets.QApplication([])
-window = QtWidgets.QPushButton("MyButton")
-window.show()
-app.exec_()
